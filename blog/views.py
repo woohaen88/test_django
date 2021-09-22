fcb09dd2d51c964a8f9293051c6314e224e1f3e3
woohaen88 <woohaen88@gmail.com>
2021-09-23 00:41:38 +0900

    FBV

---
+++ blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from .models import Post
+# Create your views here.
+def index(request):
+    posts = Post.objects.all().order_by('-pk')
+
+    return render(request, 'blog/index.html', context={'posts': posts})
+
+def single_post_page(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    return render(request, 'blog/single_page.html', context={'post': post})
