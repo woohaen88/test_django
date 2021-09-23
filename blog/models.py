6b1aa59c22fcbd531c005ce52c9b5d2e75953e8a
woohaen88 <woohaen88@gmail.com>
2021-09-23 20:15:59 +0900

    Category 모델추가

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,17 @@ import os
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=200, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
 class Post(models.Model):
     title = models.CharField(max_length=50)
     hook_text = models.CharField(max_length=100, blank=True)
@@ -16,6 +27,7 @@ class Post(models.Model):
     # author: 추후
     
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL, blank=True)
     def __str__(self):
         return f'[{self.pk}] {self.title} :: {self.author}'
 
@@ -28,3 +40,4 @@ class Post(models.Model):
 
     def get_file_ext(self):
         return self.get_file_name().split('.')[-1] 
+
