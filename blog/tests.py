8feb2b6725fd29da04ea401175bcac37079ea749
woohaen88 <woohaen88@gmail.com>
2021-09-23 20:02:56 +0900

    포스트 목록, 상세 페이지에서 작성자 정보 출력하기

--- blog/tests.py
+++ blog/tests.py
@@ -2,11 +2,20 @@ from django.test import TestCase
 from django.test.client import Client
 from bs4 import BeautifulSoup
 from .models import Post
+from django.contrib.auth.models import User
 
 # Create your tests here.
 class TestView(TestCase):
     def setUp(self):
         self.client = Client()
+        self.user_trump = User.objects.create_user(
+            username = 'trump',
+            password = 'somepassword'
+        )
+        self.user_obama = User.objects.create_user(
+            username = 'obama',
+            password = 'somepassword'
+        )
 
     def navbar_text(self, soup):
         navbar = soup.nav
@@ -54,11 +63,13 @@ class TestView(TestCase):
         # 3.1 만약 게시물이 2개 잇다면,
         post_001 = Post.objects.create(
             title = '첫번째 포스트 입니다.',
-            content = 'Hello, World, We are the World',)
+            content = 'Hello, World, We are the World',
+            author=self.user_trump)
 
         post_002 = Post.objects.create(
             title = '두번째 포스트 입니다.',
-            content = 'Django 연습중입니다.',)
+            content = 'Django 연습중입니다.',
+            author = self.user_obama)
 
         self.assertEqual(Post.objects.count(), 2)
 
@@ -69,15 +80,19 @@ class TestView(TestCase):
         # 3.3 메인 영역에 포스트 2개의 타이틀이 존재한다.
         main_area = soup.find('div', id="main-area")
         self.assertIn(post_001.title, main_area.text)
-        self.assertIn(post_002.title, main_area.text)
+        self.assertIn(post_002.title, main_area.text)        
 
         # 3.4 "아직 게시물이 없습니다"라는 문구가 없어야 한다.
         self.assertNotIn('아직 게시물이 없습니다.', main_area.text)
 
+        self.assertIn(post_001.author.username.upper(), main_area.text)
+        self.assertIn(post_002.author.username.upper(), main_area.text)
+
     def test_post_detail(self):
         post_001 = Post.objects.create(
             title = '첫번째 포스트 입니다.',
-            content = 'Hello, World, We are the World',)
+            content = 'Hello, World, We are the World',
+            author = self.user_trump)
 
         self.assertEqual(Post.objects.count(), 1)
         self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
@@ -92,4 +107,5 @@ class TestView(TestCase):
         main_area = soup.find('div', id="main-area")
         post_area = main_area.find('div', id="post-area")
         self.assertIn(post_001.title, post_area.text)
+        self.assertIn(self.user_trump.username.upper(), post_area.text)
         self.assertIn(post_001.content, post_area.text)
