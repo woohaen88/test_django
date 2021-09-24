9ea0be51e861bf511f302b73f66d1c72eace0a3d
woohaen88 <woohaen88@gmail.com>
2021-09-24 21:25:57 +0900

    로그인 사용자에게만 포스트 작성 페이지 열어주기

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
+    path('create_post/', views.PostCreate.as_view()),
     path('tag/<str:slug>/', views.tag_page),
     path('category/<str:slug>/', views.category_page),
     path('<int:pk>/', views.PostDetail.as_view()),
--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
-    path('', include('single_pages.urls'))
+    path('', include('single_pages.urls')),    
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
